faa0187c19bd6acdecc3acf954b29e0527319998
ageiwa <ander123fakk@gmail.com>
2025-03-02 22:50:41 +0300

    Init commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module to-do-list-api
+
+go 1.23.0
+
+toolchain go1.23.6
+
+require golang.org/x/crypto v0.35.0
---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	id int
+	email string
+	hash string
+}
+
+type RegisterOptions struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+var idCounter = 0
+var users []User
+
+func errResponse(w http.ResponseWriter, message string, httpStatusCode int) {
+	w.WriteHeader(httpStatusCode)
+
+	resp := make(map[string]any)
+	resp["message"] = message
+
+	jsonResp, _ := json.Marshal(resp)
+	w.Write(jsonResp)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		errResponse(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	
+	opt := RegisterOptions{}
+	err := decoder.Decode(&opt)
+
+	if err != nil {
+		errResponse(w, "Error decode " + err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for _, user := range users {
+		if user.email == opt.Email {
+			errResponse(w, "User already exist", http.StatusBadRequest)
+			return
+		}
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(opt.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		errResponse(w, "Error hash password", http.StatusInternalServerError)
+		return
+	}
+
+	idCounter++
+
+	users = append(users, User{
+		id: idCounter,
+		email: opt.Email,
+		hash: string(hash),
+	})
+
+	w.Write([]byte(`{ "success": true }`))
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		errResponse(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	opt := RegisterOptions{}
+	err := decoder.Decode(&opt)
+
+	if err != nil {
+		errResponse(w, "Error decode " + err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	isFind := false
+	findUser := User{}
+
+	for _, user := range users {
+		if user.email == opt.Email {
+			findUser = user
+			isFind = true
+			break
+		}
+	}
+
+	if !isFind {
+		errResponse(w, "User not found", http.StatusBadRequest)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(findUser.hash), []byte(opt.Password))
+
+	if err != nil {
+		errResponse(w, "Invalid Email or password", http.StatusBadRequest)
+		return
+	}
+
+	resp := make(map[string]any)
+	resp["id"] = findUser.id
+	resp["email"] = findUser.email
+
+	jsonResp, _ := json.Marshal(resp)
+
+	w.Write(jsonResp)
+}
+
+func main() {
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
+
+	println("Server is started...")
+	http.ListenAndServe("127.0.0.1:8080", nil)
+}
