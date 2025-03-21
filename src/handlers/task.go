a6f711f0543af10059663c9f88a4e9e19811112d
ageiwa <ander123fakk@gmail.com>
2025-03-21 12:03:38 +0300

    Made auth middleware

--- src/handlers/task.go
+++ src/handlers/task.go
@@ -2,13 +2,8 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"strings"
-	"time"
 	"to-do-list-api/src/entities"
-
-	"github.com/golang-jwt/jwt/v5"
 )
 
 type CreateTaskOptions struct {
@@ -27,59 +22,11 @@ func TaskHanlder(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateTask(w http.ResponseWriter, r *http.Request) {
-	authHeader := r.Header.Get("Authorization")
-
-	if authHeader == "" {
-		errResponse(w, "Authorization header missing", http.StatusUnauthorized)
-		return
-	}
-
-	parts := strings.Split(authHeader, " ")
-
-	if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
-		errResponse(w, "Authorization header format must be Bearer {token}", http.StatusUnauthorized)
-		return
-	}
-
-	tokenString := parts[1]
-
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-
-		return []byte("my-super-sign"), nil
-	})
-
-	if err != nil || !token.Valid {
-		errResponse(w, "Invalid token", http.StatusUnauthorized)
-		return
-	}
-
-	claims, ok := token.Claims.(jwt.MapClaims)
+	userIdFloat, ok := r.Context().Value(userIdKey).(float64)
 
 	if !ok {
-		errResponse(w, "Invalid claims", http.StatusInternalServerError)
+		errResponse(w, "User id not found", http.StatusInternalServerError)
 		return
-	}
-
-	exp, ok := claims["exp"].(float64)
-
-	if !ok {
-		errResponse(w, "Wrong exp type", http.StatusUnauthorized)
-		return
-	}
-
-	if int64(exp) < time.Now().Unix() {
-		errResponse(w, "Token expired", http.StatusUnauthorized)
-		return
-	}
-
-	id, ok := claims["id"].(float64)
-
-	if !ok {
-		errResponse(w, "Wrong id type", http.StatusUnauthorized)
-		return 
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -92,7 +39,7 @@ func CreateTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	taskId, err := entities.CreateTask(opt.Title, opt.Desc, int(id))
+	taskId, err := entities.CreateTask(opt.Title, opt.Desc, int(userIdFloat))
 
 	if err != nil {
 		errResponse(w, "Cant create task: " + err.Error(), http.StatusBadRequest)
@@ -105,30 +52,22 @@ func CreateTask(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetTasks(w http.ResponseWriter, r *http.Request) {
-	userId, ok := r.Context().Value(userIdKey).(int)
+	userIdFloat, ok := r.Context().Value(userIdKey).(float64)
 
 	if !ok {
 		errResponse(w, "User id not found", http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Println(userId)
+	userId := int(userIdFloat)
+	tasks, err := entities.GetTasks(userId)
 
-	// println(userId)
+	if err != nil {
+		errResponse(w, "Cant get tasks", http.StatusBadRequest)
+		return
+	}
 
-	// if !ok {
-	// 	errResponse(w, "User id not found", http.StatusInternalServerError)
-	// 	return
-	// }
-
-	// tasks, err := entities.GetTasks(userId)
-
-	// if err != nil {
-	// 	errResponse(w, "Cant get tasks", http.StatusBadRequest)
-	// 	return
-	// }
-
-	// successResponse(w, map[string]any{
-	// 	"tasks": tasks,
-	// }, http.StatusOK)
+	successResponse(w, map[string]any{
+		"tasks": tasks,
+	}, http.StatusOK)
 }
